8b7c6d8c52192529a2fa2cfd5b0ba8bbba429127
otnk666 <128681288+otnk666@users.noreply.github.com>
2025-10-02 04:42:37 +0900

    ex00/move to directory

--- ex00/newZombie.cpp
+++
@@ -1,9 +0,0 @@
-#include "Zombie.hpp"
-
-Zombie* newZombie(std::string name)
-{
-    Zombie* zombie;
-    zombie = new Zombie();
-    zombie->set_name(name);
-    return (zombie);
-}
--- ex00/randomChump.cpp
+++
@@ -1,8 +0,0 @@
-#include "Zombie.hpp"
-
-void    randomChump(std::string name)
-{
-    Zombie stack_zombie;
-    stack_zombie.set_name(name);
-    stack_zombie.announce();
-}
